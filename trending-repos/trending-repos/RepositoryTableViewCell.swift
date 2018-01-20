55d231d978b9bfa1eec502366867e1a589b89614
Nabila Sghir <nabila.sghir@gmail.com>
2018-01-20 11:23:17 +0100

    Create TableView's Cell

---
+++ trending-repos/trending-repos/RepositoryTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  RepositoryTableViewCell.swift
+//  trending-repos
+//
+//  Created by nabila on 1/20/18.
+//  Copyright © 2018 nabila. All rights reserved.
+//
+
+import UIKit
+
+class RepositoryTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var repoName: UILabel!
+    @IBOutlet weak var repoDescription: UILabel!
+    @IBOutlet weak var ownerAvatar: UIImageView!
+    @IBOutlet weak var ownerName: UILabel!
+    @IBOutlet weak var nbStars: UILabel!
+    
+
+}
